3cca82e11ce6829f784df7d48bc20013e52e9baf
Ruslan <rnagumanov57@gmail.com>
2025-06-06 23:59:26 +0500

    fix

--- src/Pages/Preview/Preview.jsx
+++ src/Pages/Preview/Preview.jsx
@@ -3,15 +3,15 @@ import { useNavigate } from 'react-router';
 import styles from './Preview.module.css';
 
 export default function Preview() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClick = () => {
-        navigate('/main');
-    };
+  const handleClick = () => {
+    navigate('/main');
+  };
 
-    return (
-        <div className={styles.container} onClick={handleClick}>
-            <video className={styles.video} src={'/videos/preview.mp4'} autoPlay loop muted playsInline />
-        </div>
-    );
+  return (
+    <div className={styles.container} onClick={handleClick}>
+      <video className={styles.video} src={'/videos/preview.mp4'} autoPlay loop muted playsInline />
+    </div>
+  );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,36 @@
 import { useLocation } from 'react-router';
-
 import styles from './Header.module.css';
 
 const Header = () => {
     const location = useLocation();
     const isMainPage = location.pathname === '/main';
+    const isGamesPage = location.pathname === '/games';
 
     return (
-        <>
-            <div className={styles.header}>
-                <button className={`${styles.button_header} ${isMainPage ? styles.button_header_main : ''}`}>
-                    Версия для <br /> слабовидящих
-                </button>
-                <button className={`${styles.button_header} ${isMainPage ? styles.button_header_main : ''}`}>
-                    каталог <br /> экспонатов
-                </button>
-                <button className={`${styles.button_header} ${isMainPage ? styles.button_header_main : ''}`}>
-                    аудиогид <br /> по приложению
-                </button>
-                <button className={`${styles.button_header} ${isMainPage ? styles.button_header_main : ''}`}>ru</button>
-            </div>
-        </>
+        <div className={styles.header}>
+            {/* Показываем первые три кнопки везде, кроме /games */}
+            {!isGamesPage && (
+                <>
+                    <button className={`${styles.button_header} ${isMainPage ? styles.button_header_main : ''}`}>
+                        Версия для <br /> слабовидящих
+                    </button>
+                    <button className={`${styles.button_header} ${isMainPage ? styles.button_header_main : ''}`}>
+                        каталог <br /> экспонатов
+                    </button>
+                    <button className={`${styles.button_header} ${isMainPage ? styles.button_header_main : ''}`}>
+                        аудиогид <br /> по приложению
+                    </button>
+                </>
+            )}
+
+            {/* Кнопка "ru" всегда отображается с разными стилями */}
+            <button
+                className={`${styles.button_header} ${isMainPage ? styles.button_header_main : ''} 
+                ${isGamesPage ? styles.button_header_games : ''}`}
+            >
+                ru
+            </button>
+        </div>
     );
 };
 
